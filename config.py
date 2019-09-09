90c9d5ef1dbe685df450cbb6bd766a710faab823
ilaplace <msemihkilic@gmail.com>
2019-09-09 17:24:00 +0300

    dev and prod config options added

---
+++ config.py
@@ -0,0 +1,19 @@
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    SQLALCHEMY_DATABASE_URI = "sqlite:///test.db"
+    secret_key = 'super secret key'
+    SESSION_TYPE = 'filesystem'
+
+class ProductionConfig(Config):
+    SERVER = "127.0.0.1"
+    FRONTEND_URI = "https://diagnostician-frontend.herokuapp.com/"
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SERVER = "0.0.0.0"
+    FRONTEND_URI = "http://localhost:3000"
+
+class TestingConfig(Config):
+    TESTING = True
