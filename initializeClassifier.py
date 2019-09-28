8ba96688db67f5859fd30b067d50e8619ebbb3e5
ilaplace <msemihkilic@gmail.com>
2019-09-28 15:43:11 +0200

    TRAIN Function implemented

--- initializeClassifier.py
+++ initializeClassifier.py
@@ -5,6 +5,12 @@ import numpy as np
 import concurrent.futures
 import joblib
 import sys
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import balanced_accuracy_score
+from imblearn.ensemble import RUSBoostClassifier
+from imblearn.metrics import geometric_mean_score
 
 async def classifier_task():
     ''' Called from the startTraining mutation, caller of the train method'''
@@ -37,32 +43,44 @@ async def initializeClassifier(loop):
     if not (User.query.get(user_id)):
         print("No database found")
 
+    pat = Patient.query.filter_by(user_id=user_id).all()
+    
+    # y is the label matrix
+    # TODO: Check if labels can be string or not
+    y = np.arange(numberOfPatients, dtype=np.float)
+    for i, index in enumerate(y):
+        y[i] = pat[i].diagnose
+
     r = db.session.query(Patient, Feature).outerjoin(
         Feature, Patient.id == Feature.patient_id).all()
 
+    # df is the data matrix X
     df = np.arange(numberOfPatients*classifier.numberOfFeatureTypes, dtype=np.float).reshape(numberOfPatients, classifier.numberOfFeatureTypes)
 
-    #a = np.arange(15).reshape(5, 3)
     for element in df.flat:
         df.flat[int(element)] = np.float(r[int(element)].Feature.featureValue)
 
     #np.set_printoptions(threshold=sys.maxsize)
-    print(df)
     classifier = Classifier.query.filter_by(user_id=user_id).first()
     with concurrent.futures.ProcessPoolExecutor() as pool:
         classifier = Classifier.query.filter_by(user_id=user_id).first()
-        trainedClassifier = await loop.run_in_executor(pool, train, classifier,df,user_id)
+        trainedClassifier = await loop.run_in_executor(pool, train, classifier,df,y,user_id)
         classifier.classifierStatus = trainedClassifier.classifierStatus
         db.session.add(classifier)
         db.session.commit()
         return "success"
 
-# simulating a CPU bound task
-def train(classifier, df, user_id):
-    #print(df)
-    sum(i * i for i in range(10 ** 7))
-    vari = 123
-    joblib.dump(vari, user_id+'.pkl')
+
+def train(classifier, df,y, user_id):
+    ''' The main training function that runs on a seperate process'''
+    X_train, X_test, y_train, y_test = train_test_split(df, y, test_size=0.33, random_state=0)
+    base_estimator = AdaBoostClassifier(n_estimators=10)
+    rusboost = RUSBoostClassifier(n_estimators=10, base_estimator=base_estimator)
+    rusboost.fit(X_train, y_train)
+    y_pred_rusboost = rusboost.predict(X_test)
+    print('Balanced accuracy: {:.2f} - Geometric mean {:.2f}'.format(balanced_accuracy_score(y_test, y_pred_rusboost), geometric_mean_score(y_test, y_pred_rusboost)))
+    cm_rusboost = confusion_matrix(y_test, y_pred_rusboost)
+    joblib.dump(rusboost, user_id+'.pkl')
     classifier.classifierStatus = "trained"
     print("Done training")
     return classifier
