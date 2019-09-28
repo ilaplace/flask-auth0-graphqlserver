e14ca9017d27e7b385a95ad622c40e6fda024384
ilaplace <msemihkilic@gmail.com>
2019-09-28 19:58:38 +0200

    diagnose uses trained model

--- server.py
+++ server.py
@@ -8,7 +8,8 @@ from ariadne import QueryType, graphql, make_executable_schema, MutationType
 from ariadne.constants import PLAYGROUND_HTML
 from flask_sqlalchemy import SQLAlchemy
 import os
-
+import json
+import numpy as np
 
 
 import time
@@ -16,7 +17,7 @@ from graphServer import blueprint_graph
 from fileUpload import blueprint_upload
 from auth_helper import get_token_auth_header, requires_auth, AuthError
 from models import Classifier, Feature, Patient, User, db
-
+import joblib
 import random
 
 ENV_FILE = find_dotenv()
@@ -105,18 +106,48 @@ def private_scoped():
 @requires_auth
 async def diagnose():
     data = await request.data
-    print(data)
-    datastr = str(data)
+    json_string = str(data)
+    json_string = json_string[2:-1:]
+    json_string = json_string.replace("\\","")
+    print(json_string)
+    data = json.loads(json_string)
+    sorted_dict_from_user = dict()
+    dict_from_user = dict()
+    list_from_user = []
+    len_of_data = len(data)
+    needed_objects = list(data[len_of_data-1].values())[0] + 1 #burada numberOfPatient tan hasta sayısını çekiyorum.
+    # label ve numberOfPatient 1 er defa, hasta değerleri ise girilen hasta sayısı kadar geliyor. yani elimdeki veri (hasta sayısı)+2 uzunluğunda.
+    #ben bu gelen datada son eleman hariç hepsini okumak istiyorum. bu sebeple n+1 eleman okumam lazım. needed_objects okumak istediğim elemnaları temsil ediyor.
 
+    for i in range(needed_objects): #bu döngüde gelen veriden label ve ona karşılık gelen değeri eşliyor ve bunu dictinarye ekliyorum
+        for j in range(len(data[i])):
+            dic = {data[0][j]['value']: data[1][j]['value']}
+            dict_from_user.update(dic)
+            list_from_user.append(dic)
+    inserted_array = []
+    for key in sorted(dict_from_user.keys()): #bu döngüde oluşturduğum dictionary nin key lerini alfabetik olarak sıralayıp yeni bir dictionary e atıyorum
+        #print(key, ":", dict_from_user[key])
+        dic = {key: dict_from_user[key]}
+        sorted_dict_from_user.update(dic)
+        inserted_array.append(np.float(dict_from_user[key]))
+
+    #print(sorted_dict_from_user)
+    inserted_np_array = np.array(inserted_array)
+    inserted_np_array = inserted_np_array.reshape(1,-1)
+    user_id = _request_ctx_stack.top.current_user.get('sub')
+    classifier = joblib.load(user_id + ".pkl")
+
+    y_pred_rusboost = classifier.predict(inserted_np_array)
+    print(y_pred_rusboost)
     # Read the number of patients
-    index = datastr.find('numberOfPatients')
-    if(index == -1):
-        return jsonify(message="numberOfPatients could not be read")
-    numberOfPatients = int(datastr[index+len('numberOfPatients')+2])
-    resp = []
-    for x in range(numberOfPatients):
-        resp.append(random.randint(1,10))
+    # index = datastr.find('numberOfPatients')
+    # if(index == -1):
+    #     return jsonify(message="numberOfPatients could not be read")
+    # numberOfPatients = int(datastr[index+len('numberOfPatients')+2])
+    # resp = []
+    # for x in range(numberOfPatients):
+    #     resp.append(random.randint(1,10))
 
-    return jsonify(message=resp)
+    return jsonify(message=y_pred_rusboost[0])
 
 
